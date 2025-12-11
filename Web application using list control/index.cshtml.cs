de17b6b39df99b0ae03449691e28ed37bed52989
jebickson <jebisam12@gmail.com>
2025-12-11 18:55:21 +0530

    4th program

---
+++ Web application using list control/index.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+public class LanguageSelectorModel : PageModel
+{
+    [BindProperty]
+    public List<string> SelectedLanguages { get; set; }
+
+    public List<SelectListItem> LanguageOptions { get; set; }
+
+    public void OnGet()
+    {
+        LoadLanguages();
+    }
+
+    public void OnPost()
+    {
+        LoadLanguages();
+        // SelectedLanguages will be automatically bound from the form
+    }
+
+    private void LoadLanguages()
+    {
+        LanguageOptions = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "C#", Text = "C#" },
+            new SelectListItem { Value = "Java", Text = "Java" },
+            new SelectListItem { Value = "Python", Text = "Python" },
+            new SelectListItem { Value = "JavaScript", Text = "JavaScript" },
+            new SelectListItem { Value = "Go", Text = "Go" }
+        };
+    }
+}
